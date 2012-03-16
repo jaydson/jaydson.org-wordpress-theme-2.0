c0124f659066d4cc9d33d2e5d1bfda2200e28279
Jaydson Gomes <jayalemao@gmail.com>
2012-03-16 00:56:09 -0300

    Bug fix for category pages

--- loop.php
+++ loop.php
@@ -8,7 +8,7 @@
 
 <?php /* Start loop */ ?>
 <?php while (have_posts()) : the_post(); ?>
-  <?php roots_post_before(); exit; ?>
+  <?php roots_post_before(); ?>
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <?php roots_post_inside_before(); ?>
       <header>
